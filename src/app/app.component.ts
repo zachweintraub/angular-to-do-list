98abda464306f8c4d7521f0c1ddd777d11902ee0
crystal fecteau and zach weintraub <crystal+zach@>
2019-06-10 10:53:20 -0700

    set up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'crap-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'To-Do List';
+  currentFocus: string = "Stay Focused Yo";
+  currentDate = new Date();
+  month: number = this.currentDate.getMonth() + 1;
+  day: number = this.currentDate.getDate();
+  year: number = this.currentDate.getFullYear();
+
+  tasks: Task[] = [
+    new Task("Get a job you hobo", new Date(2019,5,14)),
+    new Task("Eat Lunch", new Date(2109,5,10)),
+    new Task("Take a nap", new Date(2019,5,10))
+  ] 
+  
 }
+
+
